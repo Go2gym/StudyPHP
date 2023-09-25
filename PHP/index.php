af35273bb7637e302f07a844184798b02c6db423
BubblingSODA <go2gym365@gmail.com>
2023-09-25 11:59:29 +0000

    PHP start

---
+++ PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>WEB</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <h2>Welcome</h2>
+    개씨발 웹 존나 할거많네
+</body>
+</html>
